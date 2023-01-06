5080ee6aea28454bddebe4fe9fdb3b0b2f0a9cba
whimy <114148910+whimy@users.noreply.github.com>
2023-01-06 17:23:37 +0800

    Update backprop.h

--- inc/backprop.h
+++ inc/backprop.h
@@ -2,5 +2,12 @@
 #define __BCAKPROP_H__
 
 #define LENGTH 5
+#define GUESSTURNS 6
+#define ALPHBET 26
+
+void feed_input(char* container);
+void pick_puzzle(FILE* fp, char* answer);
+FILE* save_data(FILE* dfp, char* vocabulary, int guessNum);
+void clearup();
 
 #endif
