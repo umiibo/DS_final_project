8dfeb104ecfd51cc694f05a43ec761b34520a1e1
whimy <114148910+whimy@users.noreply.github.com>
2023-01-06 17:22:51 +0800

    Update file.c

--- src/file.c
+++ src/file.c
@@ -15,24 +15,6 @@ FILE* open_library(FILE* fp){
 }
 
 FILE* tidy_up(FILE* fplib){
-    char* buffer;
-    buffer = (char*)malloc(sizeof(char)*LENGTH);
-
-    // if(!(fplib = fopen("martin.txt","r"))){
-    //     // printf("Fail to open the file.\n");
-    // }else{
-    //     // printf("Open the file successfully.\n");
-    // }
     
-    // rewind(fplib);
-    // srand(time(NULL));
-    // int order = rand() % 1003;
-    fseek(fplib,3,SEEK_SET);
-    fseek(fplib, 3 * (sizeof(char)*5), SEEK_CUR);
-
-    fscanf(fplib, "%s", buffer);
-
-    printf("%s\n",buffer);
-
     return fplib;
 }
