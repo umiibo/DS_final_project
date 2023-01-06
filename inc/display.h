41005ddb48a23eff131cb7c33a2a909c83d0a9e2
whimy <114148910+whimy@users.noreply.github.com>
2023-01-06 17:23:17 +0800

    Update display.h

--- inc/display.h
+++ inc/display.h
@@ -1,10 +1,12 @@
 #ifndef __DISPLAY_H__
 #define __DISPLAY_H__
 
+#include "backprop.h"
+
 enum keyboard_status_t{
     UNUSED, NONEXIST, WRONGSPOT, CORRECT
 };
 
-void print_Board(char** word);
-void print_KB(int kb[26]);
+void print_Board(char** word, int status[GUESSTURNS][LENGTH]);
+void print_KB(int kb[]);
 #endif
