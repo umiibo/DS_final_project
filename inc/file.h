096a2106b14434a06d935417ef5e6033b11e0203
whimy <114148910+whimy@users.noreply.github.com>
2023-01-09 17:55:23 +0800

    Update file.h

--- inc/file.h
+++ inc/file.h
@@ -1,6 +1,27 @@
 #ifndef __FILE_H__
 #define __FILE_H__
 
+
+#define SIZEOF_LINE_IN_FILE 8
+struct node{
+    int turn;
+    char voc[5];
+    struct node *next;
+};
+typedef struct node NODE;
+
+typedef struct trie_t{
+    union{
+        struct trie_t *kids[ALPHBET];
+        int guess_order[2];
+    };
+    bool isLeaf;
+}Trie;
+
+
 FILE* open_library(FILE* fp);
-FILE* tidy_up(FILE* fplib);
+FILE* write_file(NODE* top, FILE* dfp);
+Trie *trieCreate();
+void trieInsert(Trie *obj, char word[LENGTH],int gtime, int index);
+int trieSearch(Trie *obj, char word[5]);
 #endif
